89eb51636fa1d8b701d59aa261b698e9bf4b55b6
calin2244 <bastureacalin@gmail.com>
2023-01-08 04:08:55 +0200

    Five day forecast

--- scripts/fiveDayForecast.js
+++ scripts/fiveDayForecast.js
@@ -1,3 +1,30 @@
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday", "Sunday","Monday","Tuesday","Wednesday", "Thursday"];
+
+//INITIALIZARI
+const headerWords = ["Waiting", "for", "your", "input", "❗❗"]
+
+for(let i = 0; i < 5; ++i){
+  document.querySelector("#weekDay" + (i+1)).textContent = headerWords[i];
+  document.querySelector("#weekImg" + (i+1)).src = "";
+  document.querySelector("#day" + (i+1) + "Min").textContent = "";
+  document.querySelector("#day" + (i+1) + "Max").style.paddingTop = "15px";
+  document.querySelector("#day" + (i+1) + "Max").textContent = "";
+
+  let loadingEl = document.createElement("div");
+  loadingEl.style.paddingTop = "40px";
+  loadingEl.style.fontFamily = "Josefin Sans";
+  loadingEl.style.fontSize = "24px";
+  loadingEl.style.width = "60px";
+  loadingEl.className = "help";
+  loadingEl.textContent = "your";
+
+  if(i === 2){
+    document.querySelector("#day" + (i+1) + "Max").appendChild(loadingEl);
+    document.querySelector("#weekDay" + (i+1)).textContent = "";
+  }
+
+}
+
 const getForecast = (city) => {
   console.log("merge:))");
   fetch("https://api.openweathermap.org/data/2.5/forecast?q="
@@ -16,19 +43,31 @@ const getForecast = (city) => {
         //   date.getYear() === currentDate.getYear()
 
         // );
-        for(var i = 0; i < 5; ++i){
-          var minTemp = data.list[i].main.temp_min.toFixed(1);
-          var maxTemp = data.list[i].main.temp_max.toFixed(1);
+
+        const d = new Date();
+        let currentDay = weekday[d.getDay()];
+
+        for(let i = 0; i < 5; ++i){
+          let minTemp = data.list[i].main.temp_min.toFixed(1);
+          let maxTemp = data.list[i].main.temp_max.toFixed(1);
           if(minTemp.charAt(minTemp.length - 1) === '0')
             minTemp = Math.floor(minTemp);
           if(maxTemp.charAt(maxTemp.length - 1) === '0')
             maxTemp = Math.floor(maxTemp);
-          document.querySelector("#day" + (i+1).toString() + "Min").innerHTML = "Min: " + minTemp + "℃";
-          document.querySelector("#day" + (i+1).toString() + "Max").innerHTML = "Max: " + maxTemp + "℃";
+          
+          const { icon } = data.list[i].weather[0];
+
+          document.querySelector("#weekDay" + (i+1)).textContent = currentDay;
+          currentDay = weekday[d.getDay() + i + 1];
+
+          document.querySelector("#weekImg" + (i+1)).src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+          document.querySelector("#day" + (i+1) + "Min").textContent = "Min: " + minTemp + "℃";
+          document.querySelector("#day" + (i+1) + "Max").textContent = "Max: " + maxTemp + "℃";
         }
 
 
-      // Display the filtered forecast data
+      //Display data
       console.log(fiveDayForecast);
     });
 };
